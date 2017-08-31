d7fa24d5cbbfe64e2d440713d8ac83080c84ea9a
Danny Rodriguez <dannidanna01@gmail.com>
2017-08-30 23:55:45 -0400

    Primer commit, coneccion Firebase - App android

---
+++ Warmi/app/src/main/java/app/warmi/rodriguez/danny/warmi/MainActivity.java
@@ -0,0 +1,40 @@
+package app.warmi.rodriguez.danny.warmi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import app.warmi.rodriguez.danny.warmi.Objects.FirebaseReferences;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference reference = database.getReference(FirebaseReferences.FIRE_REFERENCE);//REFERENCIA
+        reference.addValueEventListener(new ValueEventListener() {  //OBTENEMOS SU VALOR
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                int value = dataSnapshot.getValue(Integer.class);
+                Log.i("DATO", value+"");
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {  // cuando se cancela, por alguna razon no podemos obtener el valor
+                Log.e("ERROR", databaseError.getMessage());
+            }
+        })
+
+
+    }
+}
---
+++ Warmi/app/src/main/java/app/warmi/rodriguez/danny/warmi/Objects/FirebaseReferences.java
@@ -0,0 +1,10 @@
+package app.warmi.rodriguez.danny.warmi.Objects;
+
+/**
+ * Created by danni on 30-Aug-17.
+ */
+
+public class FirebaseReferences {
+
+    public final static String FIRE_REFERENCE = "IdComplaint";
+}
